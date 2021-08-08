c96a7a149c7b9a77bc4c49584c81e2d0f37096fc
Vadim Shpet <vadik.shpet01@gmail.com>
2021-08-08 11:56:27 +0300

    refactoring code with method 'map'

--- src/components/Content/Messages/Companions/Companions.jsx
+++ src/components/Content/Messages/Companions/Companions.jsx
@@ -2,14 +2,18 @@ import Companion from './Companion/Companion';
 import mod from './Companions.module.css';
 
 const Companions = () => {
+    const companionsData = [
+        { id: "1", imgUrl: "https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo", imgAlt: "companion's avatar", name: "Vadik Shpet" },
+        { id: "2", imgUrl: "https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo", imgAlt: "companion's avatar", name: "Pomidor Siniy" },
+        { id: "3", imgUrl: "https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo", imgAlt: "companion's avatar", name: "Pochka Ruka" },
+        { id: "4", imgUrl: "https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo", imgAlt: "companion's avatar", name: "Lishayna Masha" },
+        { id: "5", imgUrl: "https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo", imgAlt: "companion's avatar", name: "Neznayeva Ksu" }
+    ];
+    let companions = companionsData.map(c => <Companion id={c.id} imgUrl={c.imgUrl} imgAlt={c.imgAlt} name={c.name} />)
     return (
 
         <div className={mod.companionWrap}>
-            <Companion id="1" imgUrl="https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo" imgAlt="companion's avatar" name="Vadik Shpet" />
-            <Companion id="2" imgUrl="https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo" imgAlt="companion's avatar" name="Pomidor Siniy" />
-            <Companion id="3" imgUrl="https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo" imgAlt="companion's avatar" name="Pochka Ruka" />
-            <Companion id="4" imgUrl="https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo" imgAlt="companion's avatar" name="Lishayna Masha" />
-            <Companion id="5" imgUrl="https://lh3.googleusercontent.com/ogw/ADea4I6pTMZmY51r32eT9i3FAiOVlAD9s9LICrcc-ifpaw=s32-c-mo" imgAlt="companion's avatar" name="Neznayeva Ksu" />
+            {companions}
         </div>
     )
 }
--- src/components/Content/Profile/MyPosts/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import Post from './Post/Post'
 import mod from './MyPosts.module.css';
 
 const MyPosts = () => {
+    const postData = [
+        { mess: "he href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles.", date: "01.03.2018" },
+        { mess: "To ignore, add // eslint-disable-next-line to the line before.", date: "31.02.2017" },
+        { mess: "First post", date: "01.06.2016" }
+    ];
+    let posts = postData.map(p => <Post mess={p.mess} date={p.date} />);
     return (
         <div className={mod.posts}>
             <h3>My posts</h3>
@@ -12,11 +18,7 @@ const MyPosts = () => {
                 </form>
             </div>
             <div className={mod['old-posts']}>
-                <Post mess="he href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. 
-                If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles."
-                    date="01.03.2018" />
-                <Post mess="To ignore, add // eslint-disable-next-line to the line before." date="31.02.2017" />
-                <Post mess="First post" date="01.01.2016" />
+                {posts}
             </div>
         </div>
     )
