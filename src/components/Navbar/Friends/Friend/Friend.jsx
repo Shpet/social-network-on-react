3972b657062f83cf5c253bd45d90c590d8d7daef
Vadim Shpet <vadik.shpet01@gmail.com>
2021-08-23 22:00:52 +0300

    created <Friends> in <Navbar>

--- src/components/Navbar/Friends/Friend/Friend.jsx
+++ src/components/Navbar/Friends/Friend/Friend.jsx
@@ -1,14 +1,16 @@
+import mod from './Friend.module.css'
 
 const Friend = props => {
 
     return (
-        <div>
-            <ul>
-                <li>{props.id}</li>
-                <li>{props.name}</li>
-                <li>{props.imgUrl}</li>
-                <li>{props.state}</li>
-            </ul>
+        <div key={props.id} className={mod.friend} >
+            <div className={mod.friend__avatarWrap}>
+                <img src={props.imgUrl} alt={props.name} />
+                {props.state &&
+                    <span className={mod.friend_online}></span>
+                }
+            </div>
+            <p className={mod.friend__name}>{props.name}</p>
         </div>
     )
 }
--- src/components/Navbar/Friends/Friends.jsx
+++ src/components/Navbar/Friends/Friends.jsx
@@ -1,11 +1,15 @@
 import Friend from './Friend/Friend';
 import mod from './Friends.module.css'
 const Friends = (props) => {
-    let friends = props.data.map(c => <Friend id={c.id} name={c.name} imgUrl={c.imgUrl} state={c.isOnline} />);
+
+    let friends = props.data
+        .sort((a, b) => b.isOnline - a.isOnline)
+        .map(c => <Friend id={c.id} name={c.name} imgUrl={c.imgUrl} state={c.isOnline} />);
 
     return (
 
         <div className={`${props.className} ${mod.friendsWrap}`}>
+
             {friends}
         </div>
     )
