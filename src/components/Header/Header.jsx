da07d62ad1b96ca62f689fb097fd8b7b72a3ea1d
Vadik Shpet <vadik.shpet01@gmail.com>
2021-08-31 16:59:32 +0300

    fixed height Header and Content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import mod from './Header.module.css';
 
 const Header = () => {
     return (
-        <header>
+        <header className={mod.header}>
             <img src="https://i.pinimg.com/originals/0f/41/d0/0f41d0404ae75869088e7c150affab84.png" alt="logo" />
         </header>
     )
